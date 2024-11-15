a0edb9fcf99c65fe86fd7a54f6a32750ba22516c
SOURAV DEBNATH <sdebnath22.cse@bu.ac.bd>
2024-11-16 00:16:05 +0600

    package

---
+++ Basic OOP/package/ClassA.java
@@ -0,0 +1,6 @@
+package package1;
+public class ClassA {
+    public void display() {
+        System.out.println("welcome to ClassA");
+    }
+}
---
+++ Basic OOP/package/Test.java
@@ -0,0 +1,8 @@
+import package1.ClassA;
+
+public class Test {
+    public static void main(String[] args) {
+        ClassA obj = new ClassA();
+        obj.display();
+    }
+}
